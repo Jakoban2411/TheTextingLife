50e16b627d1c9f0ba37c671307ab8e3cd2b31049
Konisko Bandyopadhyay <30558121+Jakoban2411@users.noreply.github.com>
2018-09-15 01:52:39 +0530

    Added an image adding function. Still need to set imagre in the scriptable object.

--- MyLife/Assets/StoryScript.cs
+++ MyLife/Assets/StoryScript.cs
@@ -6,15 +6,48 @@ using UnityEngine.UI;
 public class StoryScript : MonoBehaviour {
 
     [SerializeField] Text StoryTextComponent;
+    [SerializeField] StoryStatesObject StartingState;
+    [SerializeField] Vector2 StartDialoguePosition;
+    StoryStatesObject[] NextStates;
+    StoryStatesObject CurrentState;
+    [SerializeField] GameObject ParentImage;
+    Image NewSpeechBubbletoGenerate, NextSpeechBubble;
+    Vector2 NextPosition;
 	// Use this for initialization
 	void Start ()
     {
-        StoryTextComponent.text = ("Heyo! I got your number from our mutual friend!");
-	}
+        CurrentState = StartingState;
+        StoryTextComponent.text = CurrentState.GetStateDialogue();
+        NextStates = CurrentState.GetNextStates();
+        NextPosition = StartDialoguePosition + new Vector2(0, 50);
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            CurrentState = NextStates[0];
+            NextStates = CurrentState.GetNextStates();
+            NextSpeechBubble = CurrentState.GetDialogueImage();
+            NewSpeechBubbletoGenerate = ParentImage.AddComponent<Image>();
+            NewSpeechBubbletoGenerate.sprite = NextSpeechBubble.sprite;
+            NewSpeechBubbletoGenerate.GetComponent<RectTransform>().SetParent(ParentImage.transform);
+            NewSpeechBubbletoGenerate.transform.position = NextPosition;
+            NextPosition =NextPosition+ new Vector2(0, 50);
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                CurrentState = NextStates[1];
+                NextStates = CurrentState.GetNextStates();
+            }
+            else
+            {
+                CurrentState = NextStates[2];
+                NextStates = CurrentState.GetNextStates();
+            }
+        }
+    }
 }
---
+++ MyLife/Assets/StoryStatesObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "State")]
+public class StoryStatesObject : ScriptableObject
+{
+    [SerializeField] Image DialogueBubble;
+    [TextArea(10, 15)] [SerializeField] string Dialogue;
+    [SerializeField] StoryStatesObject[] NextStates;
+    public string GetStateDialogue()
+    {
+        return Dialogue;
+    }
+    public StoryStatesObject[] GetNextStates()
+    {
+        return NextStates;
+    }
+    public Image GetDialogueImage()
+    {
+        return DialogueBubble;
+    }
+}
